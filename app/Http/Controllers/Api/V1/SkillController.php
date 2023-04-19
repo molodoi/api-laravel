68324ccf0b7cf3a782e7f35ff97983fc8c64ccf2
molodoi <mr_stat12@hotmail.com>
2023-04-19 15:02:20 +0200

    Skill Resource + Collection

--- app/Http/Controllers/Api/V1/SkillController.php
+++ app/Http/Controllers/Api/V1/SkillController.php
@@ -3,20 +3,24 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Skill;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreSkillRequest;
-use Illuminate\Http\JsonResponse;
+use App\Http\Resources\V1\SkillResource;
+use App\Http\Resources\V1\SkillCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class SkillController extends Controller
 {
-    public function index()
+    public function index(): ResourceCollection
     {
-        return Skill::all();
+        return new SkillCollection(Skill::paginate(2));
     }
 
-    public function show(Skill $skill)
+    public function show(Skill $skill): JsonResource
     {
-        return $skill;
+        return new SkillResource($skill);
     }
 
     public function store(StoreSkillRequest $request): JsonResponse
